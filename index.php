cf66892cd2370ff47690a93627257a7ff79a2734
Christian Beretta <beretta.christian2602@gmail.com>
2020-04-20 14:23:56 +0200

    installazione laravel

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-ajax-dischi</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.3/handlebars.min.js" charset="utf-8"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
