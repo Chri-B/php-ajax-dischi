f3209aff690b63cb1ac3f392f2b06eb400d30454
Christian Beretta <beretta.christian2602@gmail.com>
2020-04-20 14:56:19 +0200

    impostazione html e prima stilizzazione

--- index.php
+++ index.php
@@ -6,8 +6,37 @@
         <title>php-ajax-dischi</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.3/handlebars.min.js" charset="utf-8"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina+2&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="dist/css/app.css">
     </head>
     <body>
+        <header>
+            <div id="logo">
+                <img src="https://dl2.macupdate.com/images/icons256/33033.png?d=1511990622" alt="logo">
+            </div>
+        </header>
+        <main>
+            <div class="container">
 
+            </div>
+        </main>
+        <footer>
+            <div class="credits">
+                <p>
+                    Made with <i class="fas fa-heart"></i> by Boolean Classe #10
+                </p>
+            </div>
+        </footer>
+
+
+        <!-- <script id="card-template" type="text/x-handlebars-template">
+            <div class="card">
+
+            </div>
+        </script> -->
+
+
+        <script src="dist/js/main.js" charset="utf-8"></script>
     </body>
 </html>
